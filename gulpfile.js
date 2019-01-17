6276a647209c3064522867058e41972d6fdf1332
Dennis Baklan <dennis.baklan@gmail.com>
2019-01-17 20:50:14 +0200

    Fix #1456

--- gulpfile.js
+++ gulpfile.js
@@ -1,84 +1,69 @@
 'use strict'
 
-var gulp = require('gulp')
-var minify = require('gulp-uglify')
-var sass = require('gulp-sass')
-var browserSync = require('browser-sync').create()
-var clean = require('gulp-clean')
-//var concat = require('gulp-concat')
-var imagemin = require('gulp-imagemin')
-var autoprefixer = require('gulp-autoprefixer')
+const gulp = require('gulp')
+const browserSync = require('browser-sync').create()
+const clean = require('gulp-clean')
+const sass = require('gulp-sass')
+const minify = require('gulp-uglify')
+const imagemin = require('gulp-imagemin')
+const autoprefixer = require('gulp-autoprefixer')
+const concat = require('gulp-concat')
+const babel = require('gulp-babel')
+const gulpSequence = require('gulp-sequence')
 
-
-gulp.task('clean', function () {
-    return gulp.src('dist', {
-            read: false
-        })
-        .pipe(clean())
-})
-
-gulp.task('minify', function () {
-    return gulp.src('./src/js/**/*.js')
-        .pipe(minify())
-        .pipe(gulp.dest('./dist/js'))
-})
-
-// gulp.task('sass', function () {
-//     return gulp.src('./src/scss/*.scss')
-//         .pipe(sass())
-//         .pipe(autoprefixer({
-//             browsers: ['last 2 versions'],
-//             cascade: false
-//         }))
-//         .pipe(gulp.dest('./dist/css'))
-// })
-
-gulp.task('sass', function () {
-    return gulp.src('./src/scss/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        // .pipe(minify().on('error', minify.logError))
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions'],
-            cascade: false
-        }))
-        .pipe(gulp.dest('./dist/css'))
-})
-
-
-gulp.task('imagemin', function () {
-    return gulp.src('./src/img/*')
-        .pipe(imagemin({
-            interlaced: true,
-            progressive: true,
-            svgoPlugins: [{
-                removeViewBox: false
-            }]
-        }))
-        .pipe(gulp.dest('./dist/img'))
-})
-
-
-
-// gulp.task('autoprefixer', () =>
-//     gulp.dest('./dist/css')
-//     .pipe(autoprefixer({
-//         browsers: ['last 2 versions'],
-//         cascade: false
-//     }))
-//     .pipe(gulp.dest('./dist/css'))
-// );
-
-
-gulp.task('serve', function () {
-    browserSync.init({
-        server: "./"
+gulp.task('clean', () =>
+ 	gulp.src('./dist', {
+      read: false
     })
-    gulp.src('./src/fonts/*').pipe(gulp.dest('./dist/fonts'));
-    gulp.watch('./src/scss/*.scss', ['sass']).on('change', browserSync.reload);
-    gulp.watch('./src/js/*.js', ['minify']).on('change', browserSync.reload);
-    gulp.watch('./index.html').on('change', browserSync.reload);
+    .pipe(clean())
+)
+
+gulp.task('minjs', () =>
+ gulp.src('./src/js/**/*.js')
+ 	.pipe(babel({
+  	presets: ['@babel/env']
+  }))
+  .pipe(minify())
+  .pipe(concat('script.min.js'))
+  .pipe(gulp.dest('./dist/js'))
+)
+
+gulp.task('sass', () =>
+ gulp.src('./src/scss/*.scss')
+		.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+		.pipe(autoprefixer({
+	    browsers: ['last 2 versions'],
+	    cascade: false
+		}))
+		.pipe(concat('style.min.css'))
+		.pipe(gulp.dest('./dist/css'))
+)
+
+gulp.task('imagemin', () =>
+	gulp.src('./src/img/*')
+	  .pipe(imagemin({
+	    interlaced: true,
+	    progressive: true,
+	    svgoPlugins: [{
+	        removeViewBox: false
+	    }]
+	  }))
+	  .pipe(gulp.dest('./dist/img'))
+)
+
+gulp.task('makedist', ['sass', 'minjs', 'imagemin'], () => {
+	gulp.src('./src/fonts/*').pipe(gulp.dest('./dist/fonts'))
+
 })
 
-gulp.task('default', ['serve'], function(){
-  console.log('=== ALL DONE ===')
-});
+gulp.task('devserver', ['makedist'], () => {
+	browserSync.init({
+    server: "./"
+	})
+	gulp.watch('./src/js/**/*.js', ['minjs']).on('change', browserSync.reload)
+	gulp.watch('./src/scss/*.scss', ['sass']).on('change', browserSync.reload)
+	gulp.watch('./index.html').on('change', browserSync.reload)
+})
+
+gulp.task('build', gulpSequence('clean', 'makedist'))
+gulp.task('dev', gulpSequence('clean', 'devserver'))
